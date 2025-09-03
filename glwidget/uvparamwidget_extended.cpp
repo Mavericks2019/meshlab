1b1d0a8512c23befb53e967795840f116f2f6b25
root <root@DESKTOP-DT896MI.localdomain>
2025-09-03 21:38:26 +0800

    add distor

--- glwidget/uvparamwidget_extended.cpp
+++ glwidget/uvparamwidget_extended.cpp
@@ -9,6 +9,19 @@ UVParamWidgetExtended::UVParamWidgetExtended(QWidget *parent)
     // 初始化任何额外的成员变量
 }
 
+void UVParamWidgetExtended::init()
+{
+	Mesh_doubleIO::copy_mesh(mesh, new_mesh);
+
+	if (!get_para_mesh()) return;
+	
+	new_para.add_property(e_segment);
+	for (auto e_h : new_para.edges())
+	{
+		new_para.property(e_segment, e_h) = -1;
+	}
+}
+
 bool UVParamWidgetExtended::get_para_mesh()
 {
     origin_para.clear();
@@ -122,4 +135,116 @@ bool UVParamWidgetExtended::get_para_mesh()
         BB_Min = BB_Min.minimize(p);
     }
     return true;
+}
+
+void UVParamWidgetExtended::calc_distortion(bool silence)
+{
+	int nf = mesh.n_faces();
+
+	int n_pos = 0;
+	int n_neg = 0;
+	double total_area = 0.0;
+	double total_uv_area = 0.0;
+
+	OpenMesh::Vec3d mesh_BB_max, mesh_BB_min;
+	std::vector<double> face_area(nf);
+	mesh_BB_max = mesh_BB_min = mesh.point(mesh.vertex_handle(0));
+	for (auto f_h : mesh.faces())
+	{
+		OpenMesh::Vec3d mesh_p[3];
+		OpenMesh::Vec3d para_p[3];
+
+		auto fh_iter = mesh.cfh_begin(f_h);
+		for (int i = 0; i < 3; i++, fh_iter++)
+		{
+			mesh_p[i] = mesh.point(mesh.to_vertex_handle(*fh_iter));
+			para_p[i] = origin_para.point(origin_para.to_vertex_handle(get_mesh2para(*fh_iter)));
+
+			mesh_BB_max = mesh_BB_max.maximize(mesh_p[i]);
+			mesh_BB_min = mesh_BB_min.minimize(mesh_p[i]);
+		}
+
+		mesh_p[1] -= mesh_p[0];
+		mesh_p[2] -= mesh_p[0];
+		para_p[1] -= para_p[0];
+		para_p[2] -= para_p[0];
+
+		face_area[f_h.idx()] = OpenMesh::cross(mesh_p[1], mesh_p[2]).norm() / 2.0;
+
+		total_area += face_area[f_h.idx()];
+		total_uv_area += OpenMesh::cross(para_p[1], para_p[2]).norm() / 2.0;
+	}
+
+	double factor = std::sqrt(total_uv_area / total_area);
+	std::vector<double> distor(nf);
+	for (auto f_h : mesh.faces())
+	{
+		OpenMesh::Vec3d mesh_p[3];
+		OpenMesh::Vec3d para_p[3];
+
+		auto fh_iter = mesh.cfh_begin(f_h);
+		for (int i = 0; i < 3; i++, fh_iter++)
+		{
+			mesh_p[i] = mesh.point(mesh.to_vertex_handle(*fh_iter));
+			para_p[i] = origin_para.point(origin_para.to_vertex_handle(get_mesh2para(*fh_iter)));
+		}
+
+		Eigen::Matrix2d mesh_M, para_M;
+
+		mesh_p[1] -= mesh_p[0];
+		mesh_p[2] -= mesh_p[0];
+		para_p[1] -= para_p[0];
+		para_p[2] -= para_p[0];
+
+		mesh_p[1] *= factor;
+		mesh_p[2] *= factor;
+
+		OpenMesh::Vec3d e1 = mesh_p[1].normalized();
+		OpenMesh::Vec3d e2 = OpenMesh::cross(mesh.normal(f_h), e1);
+
+		mesh_M(0, 0) = mesh_p[1].norm();
+		mesh_M(1, 0) = 0.0;
+		mesh_M(0, 1) = OpenMesh::dot(mesh_p[2], e1);
+		mesh_M(1, 1) = OpenMesh::dot(mesh_p[2], e2);
+
+		para_M(0, 0) = para_p[1][0];
+		para_M(1, 0) = para_p[1][1];
+		para_M(0, 1) = para_p[2][0];
+		para_M(1, 1) = para_p[2][1];
+
+		double det_p = para_M.determinant();
+
+		(det_p > 0 ? n_pos : n_neg)++;
+
+		if (det_p <= 0)
+		{
+			para_M.row(0) = -para_M.row(0);
+			flipped_faces.insert(f_h.idx());
+		}
+
+		Eigen::Matrix2d J = para_M * mesh_M.inverse();
+		Eigen::JacobiSVD<Eigen::Matrix2d> SVD_solver;
+
+		SVD_solver.compute(J);
+		Eigen::Vector2d singulars = SVD_solver.singularValues();
+
+		double det_J = J.determinant();
+		double s_max = singulars.maxCoeff();
+		double s_min = singulars.minCoeff();
+
+		distor[f_h.idx()] = (s_max * s_max + s_min * s_min + 1.0 / s_max / s_max + 1.0 / s_min / s_min) * 0.25;
+	}
+
+	double x_avg_w = 0.0;
+	for (int i = 0; i < distor.size(); i++) x_avg_w += distor[i] * face_area[i];
+	x_avg_w /= total_area;
+
+	para_distortion = x_avg_w;
+
+	// if (!silence)
+	// {
+	// 	std::cout << "PE " << total_uv_area / viewer->get_bb_area() << std::endl;
+	// 	std::cout << "BL " << cut_length / (mesh_BB_max - mesh_BB_min).norm() << std::endl;
+	// 	std::cout << "ED " << x_avg_w << std::endl;
+	// }
 }
--- glwidget/uvparamwidget_extended.h
+++ glwidget/uvparamwidget_extended.h
@@ -11,8 +11,15 @@ class UVParamWidgetExtended : public UVParamWidget
 public:
     explicit UVParamWidgetExtended(QWidget *parent = nullptr);
     virtual ~UVParamWidgetExtended() = default;
+    void init();
 
     bool get_para_mesh();
+    void calc_distortion(bool silence);
+	OpenMesh::HalfedgeHandle get_mesh2para(OpenMesh::HalfedgeHandle h_mesh)
+	{
+		const auto& h_pair = new_mesh.property(h_mesh2para, h_mesh);
+		return new_para.find_halfedge(new_para.vertex_handle(h_pair.first), new_para.vertex_handle(h_pair.second));
+	};
 
 protected:
     Mesh origin_para;
@@ -21,7 +28,10 @@ protected:
     std::vector<int> origin_h_mesh2para;
     OpenMesh::Vec3d BB_Max, BB_Min;
     OpenMesh::HPropHandleT<std::pair<int, int>> h_mesh2para, h_para2mesh;
+    OpenMesh::EPropHandleT<int> e_segment;
     double cut_length = 0.0;
+    std::set<int> flipped_faces;
+    double para_distortion = 0.0;
 };
 
 #endif // UVPARAMWIDGET_EXTENDED_H
