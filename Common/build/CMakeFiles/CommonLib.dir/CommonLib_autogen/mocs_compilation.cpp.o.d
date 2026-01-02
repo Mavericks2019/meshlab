aea8032924216a4b5c729d7b67beb19e13ea71de
root <root@DESKTOP-DT896MI.localdomain>
2026-01-02 20:20:32 +0800

    add

--- Common/build/CMakeFiles/CommonLib.dir/CommonLib_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/CommonLib.dir/CommonLib_autogen/mocs_compilation.cpp.o: \
- /opt/meshoperator/Common/build/CommonLib_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
--- Cutting/build/CMakeFiles/QGLViewerWidget_autogen_timestamp_deps.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/QGLViewerWidget_autogen_timestamp_deps"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/QGLViewerWidget_autogen_timestamp_deps.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
